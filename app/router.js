1f53423486808cc7d3f68bc4f4f21106504f1e9a
David Pineda <david.pineda.codes@gmail.com>
2023-09-02 15:04:47 -0400

    hide sample routes

--- app/router.js
+++ app/router.js
@@ -7,9 +7,9 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
-  this.route('clothes', function () {
-    this.route('shirts');
-  });
+  // this.route('clothes', function () {
+  //   this.route('shirts');
+  // });
   this.route('item', { path: '/item/:item_id' });
   this.route('not-found', { path: '/*path' });
 });
